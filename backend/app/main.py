72293753b06b2dadfe3768118a1cefe207266a57
SylviaGit4 <torchicky139@gmail.com>
2026-01-20 16:14:56 +0000

    Commented imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,19 @@
-from flask import Flask, url_for, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
-from flask_login import LoginManager, login_required, login_user
-from pathlib import Path
+from flask import Flask, url_for, render_template, redirect 
+from flask_cors import CORS # Added to allow other files to call the app
+from flask_wtf import FlaskForm # For creating forms
+from wtforms import StringField, SubmitField, PasswordField # For creating forms
+from wtforms.validators import DataRequired # For creating forms
+from flask_login import LoginManager, login_required, login_user # For ensuring user login protection
+from pathlib import Path # For handling file paths
+from flask_bootstrap import Bootstrap5 # Bootstrap CSS integration
+from flask_sqlalchemy import SQLAlchemy #
 
-from flask_bootstrap import Bootstrap5
-
-import os
-from flask_sqlalchemy import SQLAlchemy
 basedir = Path.cwd()
 datadir = (basedir / "backend" / "data" / "data.sqlite")
 
 app = Flask(__name__)
+
+CORS(app)
 
 bootstrap = Bootstrap5(app)
 
