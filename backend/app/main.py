aa8ebd181a80ead0a8d71ab92de2162141b18728
SylviaGit4 <torchicky139@gmail.com>
2026-01-20 13:15:16 +0000

    Linked main.py to data.sqlite

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,18 +3,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 from flask_login import LoginManager, login_required, login_user
+from pathlib import Path
 
 from flask_bootstrap import Bootstrap5
 
 import os
 from flask_sqlalchemy import SQLAlchemy
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = Path.cwd()
+print(str(basedir / "backend" / "data" / "data.sqlite"))
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap5(app)
 
 app.config['SECRET_KEY'] = 'key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(basedir / "backend" / "data" / "data.sqlite")
 
 db = SQLAlchemy(app)
