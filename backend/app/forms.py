192d1719da32b6ef5d73526bb7ce4edadc0341a6
SylviaGit4 <torchicky139@gmail.com>
2026-02-09 23:34:01 +0000

    Moved login & registration forms into 1 file with booking forms

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -1,14 +1,25 @@
 from flask import Flask, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms import StringField, SubmitField, IntegerField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Optional
 from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
 
-class BookingForm(FlaskForm):
+class ZooBookingForm(FlaskForm):
     start_date_zoo = StringField('Start Date', validators=[DataRequired()])
     end_date_zoo = StringField('End Date', validators=[DataRequired()])
     child_tickets = IntegerField('Child Tickets', default=0, validators=[Optional()])
     adult_tickets = IntegerField('Adult Tickets', default=0, validators=[Optional()])
     educational_visit = BooleanField('Educational Visit')
     submit = SubmitField('Book')
+
+class RegistrationForm(FlaskForm):
+    username = StringField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    email = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+    submit = SubmitField("Login")
